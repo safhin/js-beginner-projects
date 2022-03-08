d6ce163fce8fa3f8bbdabc34ecf1cea31756ca46
shohel <shohel01716@gmail.com>
2022-03-08 10:31:23 +0600

    Add dynamic date and time

--- weather-app/js/app.js
+++ weather-app/js/app.js
@@ -10,8 +10,26 @@ const locationElem = document.querySelector('.location');
 const tempElem = document.querySelector('.weather-temp');
 const hTempElem = document.querySelector('.h-temp-result');
 const lTempElem = document.querySelector('.l-temp-result');
+const dayElem = document.querySelector('.date-dayname');
+const dateElem = document.querySelector('.date-day');
 
-const fahrenheitToCelsius = fahrenheit => (fahrenheit - 32) * 5/9;
+(function() {
+    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+    Date.prototype.getMonthName = function() {
+        return months[ this.getMonth() ];
+    };
+    Date.prototype.getDayName = function() {
+        return days[ this.getDay() ];
+    };
+})();
+
+var now = new Date();
+
+var day = now.getDayName();
+var month = now.getMonthName();
 
 searchBtn.addEventListener('click', function(){
     userAction().then((data) => {
@@ -23,6 +41,8 @@ searchBtn.addEventListener('click', function(){
         lTempElem.innerHTML = Math.floor(data.main['temp_min'] - 273.15) + "°C";
         visibilityElem.innerHTML = data.visibility;
         locationElem.innerHTML = data.name + " , " + data.sys['country'];
+        dayElem.innerHTML = day;
+        dateElem.innerHTML = now.getDate() + " " + month + " " + now.getFullYear();
         searchElem.value = "";
     });
 });
